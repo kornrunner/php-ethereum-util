783bb68e0463f9de0859f242509fc07ec9af5e9f
Boris Momčilović <boris@firstbeatmedia.com>
2017-12-04 18:30:12 +0100

    Eth: handle hash prefix

--- src/Eth.php
+++ src/Eth.php
@@ -4,6 +4,10 @@ namespace kornrunner;
 
 final class Eth {
     public static function hashPersonalMessage(string $message): string {
+        if (stripos($message, '0x') === 0) {
+            $message = substr($message, 2);
+        }
+
         $buffer = unpack('C*', hex2bin($message));
         $prefix = bin2hex("\u{0019}Ethereum Signed Message:\n" . sizeof($buffer));
         return Keccak::hash(hex2bin($prefix . $message), 256);
--- test/EthTest.php
+++ test/EthTest.php
@@ -4,8 +4,18 @@ use kornrunner\Eth;
 
 class EthTest extends PHPUnit\Framework\TestCase
 {
-    public function testHashPersonalMessage()
+    /**
+     * @dataProvider hashPersonalMessage
+     */
+    public function testHashPersonalMessage($message, $expect)
     {
-        $this->assertSame(Eth::hashPersonalMessage('f20f20d357419f696f69e6ff05bc6566b1e6d38814ce4f489d35711e2fd2c481'), '58a2db04c169254495a55b6dd5609a4902678ec29eac46df1e95994cdbeaebbb');
+        $this->assertSame(Eth::hashPersonalMessage($message), $expect);
+    }
+
+    public static function hashPersonalMessage(): array {
+        return [
+            ['f20f20d357419f696f69e6ff05bc6566b1e6d38814ce4f489d35711e2fd2c481', '58a2db04c169254495a55b6dd5609a4902678ec29eac46df1e95994cdbeaebbb'],
+            ['0xf20f20d357419f696f69e6ff05bc6566b1e6d38814ce4f489d35711e2fd2c481', '58a2db04c169254495a55b6dd5609a4902678ec29eac46df1e95994cdbeaebbb'],
+        ];
     }
 }
